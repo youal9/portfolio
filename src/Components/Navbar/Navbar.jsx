275f4b7412752d0b1de3671d0a300c81706eee3c
youal woldemariam <youalwol@yahoo.com>
2024-06-10 13:39:22 +0200

    done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState}from 'react'
 import { FaBars, FaTimes } from "react-icons/fa";
 import './Navbar.css'
-import logo from "../../assets/Y.png";
+import logo from "../../assets/Yw.png";
 
 
 const Navbar = () => {
