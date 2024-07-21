5c336b23eee9baa96400331bde712edc2a17f48b
youal woldemariam <youalwol@yahoo.com>
2024-07-21 21:48:56 +0200

    Added images of laptops with project screenshots in the Project component.  Linked laptop images to GitHub repository of the project

---
+++ src/Components/Projekt/Projekt.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import './Projekt.css';
+import laptop from '../../assets/laptop.png';
+import laptopApp from '../../assets/shopp.jpeg';
+
+const Project = () => {
+  return (
+    <div className="projects">
+      <h1 className="projectsTitle">Projects</h1>
+      <div className="project">
+        <div className="laptop">
+          <img src={laptop} alt="Laptop" className="laptopImage"  />
+          <div className="laptopScreen">
+            <img src={laptopApp} alt="Laptop App" className="shoppApp" />
+          </div>
+        </div>
+
+        <div className="projectDetail">
+          <h1 className="projectTitle">Webb Shop</h1>
+          <p className="projectDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            ukkjkj jjkljkl jkjklj kjkj
+           
+          </p>
+          
+          <a href="https://github.com/youal9/webbshop" target="_blank" rel="noopener noreferrer" className="githubLink">GitHub</a>
+        </div>
+      </div>
+
+      <div className="project">
+        <div className="laptop">
+          <img src={laptop} alt="Laptop" className="laptopImage"  />
+          <div className="laptopScreen">
+            <img src={laptopApp} alt="Laptop App" className="shoppApp" />
+          </div>
+        </div>
+         
+        <div className="projectDetail">
+          <h1 className="projectTitle">Webb Shop</h1>
+          <p className="projectDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            ukkjkj jjkljkl jkjklj kjkj
+           
+          </p>
+         
+        </div>
+      </div>
+
+      <div className="project">
+        <div className="laptop">
+          <img src={laptop} alt="Laptop" className="laptopImage"  />
+          <div className="laptopScreen">
+            <img src={laptopApp} alt="Laptop App" className="shoppApp" />
+          </div>
+        </div>
+         
+        <div className="projectDetail">
+          <h1 className="projectTitle">Webb Shop</h1>
+          <p className="projectDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            ukkjkj jjkljkl jkjklj kjkj
+           
+          </p>
+         
+        </div>
+      </div>
+
+
+    </div>
+    
+
+    
+  );
+};
+
+export default Project;
+
+
