ccb38dc17ccac7ad97d914b9f04fc40c53494381
youal woldemariam <youalwol@yahoo.com>
2024-07-30 15:21:09 +0200

    Add smooth scroll to all navbar links. and Implement sticky navbar that activates when scrolling past 100px

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React, {useState}from 'react'
+import React, {useEffect, useState}from 'react'
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link, scroller} from 'react-scroll';
 import './Navbar.css'
 import logo from "../../assets/yw1.png";
 
@@ -8,16 +9,35 @@ const Navbar = () => {
 
     const [click, setClick] = useState(false)
 
+    const [sticky, setSticky] = useState(false)
+
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            window.scrollY > 100 ? setSticky(true) : setSticky(false);
+        })
+
+
+         // Scrolla till "Home"-sektionen när sidan laddas
+        scroller.scrollTo('Home', {
+            duration: 500,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+          });
+    },[])
+
+    
+
     const handleClick = () =>{
         setClick(!click)
     }
 
   return (
-    <div className='header'>
+    <div className={`header ${sticky? 'dark-nav':''  }`} >
         <nav className='navbar'>
-            <a href="/" className='logo'>
-             <img src={logo} alt="logo" />
-            </a>
+        <Link to="Home" smooth={true} duration={500}  className='logo'>
+          <img src={logo} alt="logo" />
+        </Link>
+
             
             <div className="hamburger" onClick={handleClick}>
                 {click ?(<FaTimes className="icon"   />) :<FaBars className="icon"    />   }
@@ -26,17 +46,17 @@ const Navbar = () => {
             
             <ul className={click ?'nav-menu active': 'nav-menu' }>
                 <li className='nav-item'>
-                    <a href="">Home</a>
+                <Link to="Home" smooth={true} duration={500} onClick={handleClick}>Home</Link>
                 </li>
                 
                 <li className='nav-item'>
-                    <a href="">Skills</a>
+                <Link to="skills" smooth={true}  duration={500} onClick={handleClick}>Skills</Link>
                 </li>
                 <li className='nav-item'>
-                    <a href="">Projekt</a>
+                <Link to="projects" smooth={true}  duration={500} onClick={handleClick}>Projekt</Link>
                 </li>
                 <li className='nav-item'>
-                    <a href="">Contact</a>
+                <Link to="contact" smooth={true} duration={500} onClick={handleClick}>Contact</Link>
                 </li>
             </ul>
         </nav>
