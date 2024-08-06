3a7c1511db38cadb6c9f041f9a291d73bbd7573b
youal woldemariam <youalwol@yahoo.com>
2024-08-07 01:29:00 +0200

    changed background video to image

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 import './index.css'; // Importera globala stilar
 
-import Home from './Components/Home/Home'
-import Skills from './Components/Skills/Skills'
-import Projekt from './Components/Projekt/Projekt'
-import Contact from './Components/Contact/Contact'
-import video from "./assets/video.mp4";
+import Home from './Components/Home/Home';
+import Skills from './Components/Skills/Skills';
+import Projekt from './Components/Projekt/Projekt';
+import Contact from './Components/Contact/Contact';
 
 function App() {
- 
-
   return (
-    <div className="main-content">
-    <video className="video-background" src={video} autoPlay loop muted playsInline controls={false}  />
-    <Navbar/>
-    <Home />
-    <Skills />
-    <Projekt />
-    <Contact/>
-  </div>
-  )
+    <div>
+      <div className="background-image"></div> {/* Bakgrundsbild */}
+      <div className="main-content">
+        <Navbar />
+        <Home />
+        <Skills />
+        <Projekt />
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
