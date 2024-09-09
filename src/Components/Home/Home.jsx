8c222678a5b0c084706dc8bffdbb8c6bc9dd67fc
youal woldemariam <youalwol@yahoo.com>
2024-09-09 18:55:17 +0200

    new portfolio img

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import logo from "../../assets/y1.png";
-import './Home.css';
+import logo from "../../assets/yyyy.png";
+import "./Home.css";
 import cv from "../../assets/cv.pdf";
 
 const Home = () => {
-    return (
-        <section className='Home'>
-            <div className="about-content">
-                <div className="about-title">
-                    <h1>Hi, I am <span>Youal</span></h1>
-                    <p>
-                        I’m a frontend developer and student with a strong passion
-                        for building user-friendly and responsive web applications.
-                        Currently studying frontend development, I am constantly
-                        seeking new opportunities to enhance my knowledge and skills in the field.
-                    </p>
-                    <a href={cv} download="Youal cv">
-                        <button>Download CV</button>
-                    </a>
-                </div>
-                <img src={logo} alt="" className="about-image" />
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="Home">
+      <div className="about-content">
+        <div className="about-title">
+          <h1>
+            Hi, I am <span>Youal</span>
+          </h1>
+          <p>
+            I’m a frontend developer and student with a strong passion for
+            building user-friendly and responsive web applications. Currently
+            studying frontend development, I am constantly seeking new
+            opportunities to enhance my knowledge and skills in the field.
+          </p>
+          <a href={cv} download="Youal cv">
+            <button>Download CV</button>
+          </a>
+        </div>
+        <img src={logo} alt="" className="about-image" />
+      </div>
+    </section>
+  );
+};
 
 export default Home;
-
