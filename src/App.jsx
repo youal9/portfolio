4657c1d25e291444d03975c2e5366ebe2154e66f
youal <youalwol@yahoo.com>
2025-10-14 21:24:22 +0200

    lade till loading-animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-
 import Home from "./Components/Home/Home";
 import Skills from "./Components/Skills/Skills";
 import Projekt from "./Components/Projekt/Projekt";
 import Contact from "./Components/Contact/Contact";
 
 function App() {
-  return (
-    <div>
-      
-      <div className="main-content">
-        <Navbar />
-        <Home />
-        <Skills />
-        <Projekt />
-        <Contact />
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 8 sekunders loading
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
       </div>
+    );
+  }
+
+  return (
+    <div className="main-content">
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projekt />
+      <Contact />
     </div>
   );
 }
